4282fe269694b9b23462822fb20a36c567180b3e
Shane Sniteman <ssniteman@gmail.com>
2014-08-30 15:42:55 -0400

    set up TableViewController / array up next

---
+++ Mapple/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Mapple
+//
+//  Created by Shane Sniteman on 8/30/14.
+//  Copyright (c) 2014 Arthur Boia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
--- Mapple/ViewController.m
+++ Mapple/ViewController.m
@@ -10,10 +10,13 @@
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import <AFNetworking.h> 
+
+#import "TableViewController.h"
+
 @interface ViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
 
 
-// shane working now
+
 @end
 
 @implementation ViewController
@@ -23,6 +26,7 @@
     CLLocation * location;
     UITableView * events;
     UILabel * spots;
+    
     
 }
 - (instancetype)init
@@ -46,12 +50,12 @@
     mapView.userTrackingMode = YES;
     [self.view addSubview:mapView];
     
-    events = [[UITableView alloc]initWithFrame:CGRectMake(0, 320, 320, 320)];
-    events.backgroundColor = [UIColor lightGrayColor];
-    events.rowHeight = 100;
-    events.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0);
-   
-    [self.view addSubview:events];
+//    events = [[UITableView alloc]initWithFrame:CGRectMake(0, 320, 320, 320)];
+//    events.backgroundColor = [UIColor lightGrayColor];
+//    events.rowHeight = 100;
+//    events.separatorInset = UIEdgeInsetsMake(0, 0, 0, 0);
+//   
+//    [self.view addSubview:events];
 
     
     locationManager = [[CLLocationManager alloc]init];
@@ -59,17 +63,23 @@
     [locationManager requestAlwaysAuthorization];
     [locationManager startUpdatingLocation];
     
+    TableViewController * tableVenues = [[TableViewController alloc] init];
+    
+     tableVenues.view.frame = CGRectMake(0, (self.view.frame.size.height) / 2, self.view.frame.size.width, (self.view.frame.size.height) / 2);
+    
+    [self.view addSubview:tableVenues.tableView];
+    
+    
     
     // NSLog(@"%@", _venue);
     
-    spots = [[UILabel alloc] initWithFrame:(CGRectMake(40,240, 240, 40))];
-    spots.textColor = [UIColor whiteColor];
-    spots.font = [UIFont fontWithName:@"HelveticaNeue"  size:20];
-    spots.backgroundColor = [UIColor darkGrayColor];
-    spots.text = [NSString stringWithFormat:@"%@", _venue[@"resultsPage"][@"result"]];
-    [self.view addSubview: spots];
+//    spots = [[UILabel alloc] initWithFrame:(CGRectMake(40,240, 240, 40))];
+//    spots.textColor = [UIColor whiteColor];
+//    spots.font = [UIFont fontWithName:@"HelveticaNeue"  size:20];
+//    spots.backgroundColor = [UIColor darkGrayColor];
+//    spots.text = [NSString stringWithFormat:@"%@", _venue[@"resultsPage"][@"result"]];
+//    [self.view addSubview: spots];
 
-    
 
 }
 //- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -89,10 +99,8 @@
         NSLog(@"%@", urlString);
         NSURL* url = [NSURL URLWithString:urlString];
         NSURLRequest* request = [NSURLRequest requestWithURL:url];
-        //    NSData * data = [[NSData alloc] initWithContentsOfURL:[NSURL URLWithString:@"http://api.songkick.com/api/3.0/search/locations.json?location=geo:33.848977,-84.373362&apikey=o3MaOGhwkmxtNIDM"]];
-        //    _venue = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+
         NSLog(@"%@", urlString);
-        
         
         AFHTTPRequestOperation * operation = [[AFHTTPRequestOperation alloc]initWithRequest:request];
         operation.responseSerializer = [AFJSONResponseSerializer serializer];
@@ -100,12 +108,9 @@
             
             NSLog(@"%@", responseObject);
             
-            
-            
         } failure:nil];
         
         [operation start];
-
         
         
         MKCoordinateRegion region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(1.0, 1.0));
